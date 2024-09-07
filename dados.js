375a76b12b50c8b1824aae1187354e8b9abf68f3
Murilo Silva <132897250+developmurilo@users.noreply.github.com>
2024-09-06 21:27:28 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -2,22 +2,25 @@ let dados = [
     {
         titulo: "Superman",
         imagem: "https://upload.wikimedia.org/wikipedia/pt/thumb/b/be/Super-Homem.jpg/250px-Super-Homem.jpg",
-        descrição: "O Superman é um alienígena de Krypton, enviado para a Terra antes da destruição de seu planeta. Criado como Clark Kent, ele possui superpoderes como força, velocidade e voo. Trabalhando como jornalista, ele utiliza suas habilidades para proteger a humanidade como o super-herói Superman, representando os valores de justiça e esperança.",
+        descricao: "O Superman é um alienígena de Krypton, enviado para a Terra antes da destruição de seu planeta. Criado como Clark Kent, ele possui superpoderes como força, velocidade e voo. Trabalhando como jornalista, ele utiliza suas habilidades para proteger a humanidade como o super-herói Superman, representando os valores de justiça e esperança.",
         link: "https://pt.wikipedia.org/wiki/Superman"
     },
     {
         titulo: "Homem de Ferro",
         imagem: "https://upload.wikimedia.org/wikipedia/pt/thumb/b/be/Invincible_Iron_Man_Vol_2_2.jpg/250px-Invincible_Iron_Man_Vol_2_2.jpg",
-        descrição: "Bilionário, playboy e filantropo, Tony Stark é o Homem de Ferro. Ferido em uma emboscada, ele cria uma armadura de alta tecnologia para manter-se vivo e usa seus poderes para proteger o mundo. O Homem de Ferro representa a inovação, a inteligência e a responsabilidade.",
+        descricao: "Bilionário, playboy e filantropo, Tony Stark é o Homem de Ferro. Ferido em uma emboscada, ele cria uma armadura de alta tecnologia para manter-se vivo e usa seus poderes para proteger o mundo. O Homem de Ferro representa a inovação, a inteligência e a responsabilidade.",
         link: "https://pt.wikipedia.org/wiki/Homem_de Ferro"
     },
     {
         titulo: "Batman",
         imagem: "https://upload.wikimedia.org/wikipedia/pt/8/8d/Batman_por_Jim_Lee.jpg",
-        descrição: "Após testemunhar o assassinato de seus pais, Bruce Wayne dedica sua vida a combater o crime em Gotham City. Treinado em diversas artes marciais e utilizando equipamentos de alta tecnologia, o Batman é um vigilante que inspira medo nos criminosos. Ele representa a justiça, a vingança e a determinação.",
+        descricao: "Após testemunhar o assassinato de seus pais, Bruce Wayne dedica sua vida a combater o crime em Gotham City. Treinado em diversas artes marciais e utilizando equipamentos de alta tecnologia, o Batman é um vigilante que inspira medo nos criminosos. Ele representa a justiça, a vingança e a determinação.",
         link: "https://pt.wikipedia.org/wiki/Batman"
     }
 ];
+
+
+
 
 
 
