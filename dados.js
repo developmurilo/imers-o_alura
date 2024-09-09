4d6f00ae93af85c90b960e2763b6a908ada24e40
Murilo Silva <132897250+developmurilo@users.noreply.github.com>
2024-09-09 20:59:23 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -4,7 +4,7 @@ let dados = [
         imagem: "https://upload.wikimedia.org/wikipedia/pt/thumb/b/be/Super-Homem.jpg/250px-Super-Homem.jpg",
         descricao: "O Superman é um alienígena de Krypton, enviado para a Terra antes da destruição de seu planeta. Criado como Clark Kent, ele possui superpoderes como força, velocidade e voo. Trabalhando como jornalista, ele utiliza suas habilidades para proteger a humanidade como o super-herói Superman, representando os valores de justiça e esperança.",
         link: "https://pt.wikipedia.org/wiki/Superman",
-        tags: "kripton, força, clark kent, visao de raio-x, lex luthor"
+        tags: "kripton, kriptonita, força, clark kent, visao de raio-x, lex luthor cueca"
     },
     {
         titulo: "Homem de Ferro",
@@ -54,7 +54,7 @@ let dados = [
         imagem: "https://upload.wikimedia.org/wikipedia/pt/4/43/El_Chapolin_Colorado.jpg",
         descricao: "O super herói mais rápido que um raio, mais forte que um touro e mais ágil que uma lebre! O Chapolin Colorado é um herói mexicano que usa seus poderes para ajudar os mais fracos. Ele representa a esperança, a amizade e a alegria.",
         link: "https://pt.wikipedia.org/wiki/Chapolin_Colorado",
-        tags: "comédia mexico chiquinha chiles comicos superpoderes"
+        tags: "comédia mexico chiquinha chiles comicos superpoderes chaves "
       },
       {
         titulo: "Homem-Aranha",
@@ -76,7 +76,77 @@ let dados = [
         descricao: "Uma piloto da Força Aérea que ganhou poderes cósmicos após um acidente, Carol Danvers se tornou a Capitã Marvel. Com força sobre-humana, capacidade de voar e manipulação de energia, ela protege a galáxia.",
         link: "https://pt.wikipedia.org/wiki/Carol_Danvers",
         tags: "poderes cosmicos força sobre-humana vingadores kree skrulls"
-      }
+      },
+      {
+        titulo: "Silvio Santos, O Rei da TV",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/8/82/Silvio_Santos_2012.jpg", 
+        descricao: "Silvio Santos, o lendário apresentador de TV brasileiro, agora é um super-herói! Usando seu incrível poder de persuasão e uma risada inconfundível, ele luta contra o tédio e o desânimo. Sempre com dinheiro voando de seus bolsos e um microfone mágico, ele encanta e salva o mundo com prêmios inesperados. A cada 'quem quer dinheiro?', ele acende a esperança nas pessoas. Sua superforça está na sua carisma e bom humor.",
+        link: "https://pt.wikipedia.org/wiki/Silvio_Santos",
+        tags: "SBT, quem quer dinheiro, alegria, carisma, apresentador, risada mágica, microfone"
+    },
+    {
+        titulo: "Pantera Negra",
+        imagem: "https://upload.wikimedia.org/wikipedia/pt/9/9f/Black_Panther_T%27Challa.png",
+        descricao: "T'Challa é o Pantera Negra, rei de Wakanda, uma nação tecnologicamente avançada na África. Como Pantera Negra, ele possui sentidos e habilidades aprimoradas devido à erva em forma de coração e sua armadura de vibranium. Ele luta para proteger seu povo e o mundo das ameaças com honra e dignidade.",
+        link: "https://pt.wikipedia.org/wiki/Pantera_Negra_(Marvel_Comics)",
+        tags: "Wakanda, vibranium, rei, superforça, tecnologia"
+    },
+    {
+        titulo: "Hulk",
+        imagem: "https://upload.wikimedia.org/wikipedia/en/5/59/Hulk_(comics_character).png",
+        descricao: "Bruce Banner, um brilhante cientista, se transforma no poderoso Hulk, um gigante verde de força ilimitada. O Hulk é movido pela raiva e é praticamente indestrutível. Ele luta contra seus inimigos com uma força esmagadora, mas também tenta controlar sua raiva para proteger inocentes.",
+        link: "https://pt.wikipedia.org/wiki/Hulk",
+        tags: "superforça, Bruce Banner, raiva, indestrutível, gigante verde"
+    },
+    {
+        titulo: "Feiticeira Escarlate",
+        imagem: "https://upload.wikimedia.org/wikipedia/pt/f/fe/Scarlet_Witch.jpg",
+        descricao: "Wanda Maximoff, a Feiticeira Escarlate, é uma poderosa mutante com a habilidade de manipular a realidade e usar magia do caos. Ela é uma das heroínas mais poderosas do universo Marvel, lutando para proteger seus entes queridos e o mundo, mas também lutando para controlar seus poderes.",
+        link: "https://pt.wikipedia.org/wiki/Feiticeira_Escarlate",
+        tags: "magia, mutante, realidade, caos, poder cósmico"
+    },
+    {
+        titulo: "Wolverine",
+        imagem: "https://upload.wikimedia.org/wikipedia/en/4/49/Wolverine_%28Marvel_Character%29.jpg",
+        descricao: "Logan, também conhecido como Wolverine, é um mutante com poderes de cura acelerada, sentidos aguçados e garras de adamantium que emergem de suas mãos. Ele é um guerreiro feroz e implacável, com um passado sombrio, mas luta incansavelmente para proteger os outros.",
+        link: "https://pt.wikipedia.org/wiki/Wolverine_(Marvel_Comics)",
+        tags: "garras, adamantium, mutante, regeneração, X-Men"
+    },
+    {
+        titulo: "Homem-Formiga",
+        imagem: "https://upload.wikimedia.org/wikipedia/en/c/cd/Ant-Man_%28Scott_Lang%29.png",
+        descricao: "Scott Lang é o segundo Homem-Formiga, um herói com a capacidade de encolher ao tamanho de um inseto e aumentar sua força. Com sua roupa especial, ele consegue se comunicar com formigas e usar seu tamanho reduzido para infiltrar-se em áreas difíceis e derrotar inimigos poderosos.",
+        link: "https://pt.wikipedia.org/wiki/Homem-Formiga",
+        tags: "encolhimento, superforça, insetos, Scott Lang, tecnologia"
+    },
+    {
+        titulo: "Doctor Strange",
+        imagem: "https://upload.wikimedia.org/wikipedia/en/5/58/Doctor_Strange_Vol_4_2.png",
+        descricao: "Stephen Strange, um ex-neurocirurgião, se torna o Feiticeiro Supremo da Terra, protegendo o planeta de ameaças místicas e interdimensionais. Ele usa seus vastos conhecimentos de magia e artefatos místicos como o Olho de Agamotto e o Manto da Levitação para lutar contra forças sobrenaturais.",
+        link: "https://pt.wikipedia.org/wiki/Doutor_Estranho",
+        tags: "magia, místico, Feiticeiro Supremo, interdimensional, Olho de Agamotto"
+    },
+    {
+        titulo: "Deadpool",
+        imagem: "https://upload.wikimedia.org/wikipedia/en/e/ed/Deadpool_%28Wade_Wilson%29.png",
+        descricao: "Wade Wilson, também conhecido como Deadpool, é o mercenário mais insano e irreverente da Marvel. Com um fator de cura que o torna praticamente imortal e uma boca afiada cheia de piadas, ele luta contra vilões enquanto quebra a quarta parede e se diverte com seus adversários.",
+        link: "https://pt.wikipedia.org/wiki/Deadpool",
+        tags: "mercenário, regeneração, imortal, piadas, anti-herói, quarta parede"
+    },
+    {
+        titulo: "Mulher Invisível",
+        imagem: "https://upload.wikimedia.org/wikipedia/en/6/6a/Sue_Storm.png",
+        descricao: "Susan Storm Richards é a Mulher Invisível, um dos membros fundadores do Quarteto Fantástico. Ela tem a habilidade de se tornar invisível e gerar campos de força poderosos. Susan usa seus poderes para proteger seus entes queridos e o planeta contra ameaças intergalácticas.",
+        link: "https://pt.wikipedia.org/wiki/Mulher_Invis%C3%ADvel",
+        tags: "invisibilidade, campos de força, Quarteto Fantástico, poderes cósmicos"
+    },
+    {
+        titulo: "Aquaman",
+        imagem: "https://upload.wikimedia.org/wikipedia/en/8/8d/Aquaman_DC_Comics.png",
+        descricao: "Arthur Curry, o Aquaman, é o rei de Atlântida e o governante dos sete mares. Ele possui a habilidade de se comunicar com criaturas marinhas, além de superforça e resistência sobre-humanas. Ele luta para proteger tanto o mundo subaquático quanto o terrestre de ameaças.",
+        link: "https://pt.wikipedia.org/wiki/Aquaman",
+        tags: "Atlântida, superforça, comunicação marinha, tridente, oceano"
+    }
 ];
 
 
